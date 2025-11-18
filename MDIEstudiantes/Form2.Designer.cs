8ed947ba71774bd694d4ea22c36ec1bea43daab9
Sadrac03 <jaretysadrac@gmail.com>
2025-11-18 08:54:56 -0600

    Add files via upload

---
+++ MDIEstudiantes/Form2.Designer.cs
@@ -0,0 +1,173 @@
+﻿namespace MDIEstudiantes
+{
+    partial class Form2
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lblCarnet = new System.Windows.Forms.Label();
+            this.lblNombre = new System.Windows.Forms.Label();
+            this.lblAsignaturas = new System.Windows.Forms.Label();
+            this.txtCarnet = new System.Windows.Forms.TextBox();
+            this.txtNombre = new System.Windows.Forms.TextBox();
+            this.dgvAsignaturas = new System.Windows.Forms.DataGridView();
+            this.colAsignatura = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.colNotas = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.btnGuardar = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvAsignaturas)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // lblCarnet
+            // 
+            this.lblCarnet.AutoSize = true;
+            this.lblCarnet.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(128)))), ((int)(((byte)(128)))));
+            this.lblCarnet.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblCarnet.Location = new System.Drawing.Point(95, 71);
+            this.lblCarnet.Margin = new System.Windows.Forms.Padding(6, 0, 6, 0);
+            this.lblCarnet.Name = "lblCarnet";
+            this.lblCarnet.Size = new System.Drawing.Size(104, 29);
+            this.lblCarnet.TabIndex = 0;
+            this.lblCarnet.Text = "Carnet :";
+            // 
+            // lblNombre
+            // 
+            this.lblNombre.AutoSize = true;
+            this.lblNombre.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(128)))), ((int)(((byte)(128)))));
+            this.lblNombre.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblNombre.Location = new System.Drawing.Point(669, 69);
+            this.lblNombre.Margin = new System.Windows.Forms.Padding(6, 0, 6, 0);
+            this.lblNombre.Name = "lblNombre";
+            this.lblNombre.Size = new System.Drawing.Size(121, 29);
+            this.lblNombre.TabIndex = 1;
+            this.lblNombre.Text = "Nombre :";
+            // 
+            // lblAsignaturas
+            // 
+            this.lblAsignaturas.AutoSize = true;
+            this.lblAsignaturas.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(128)))), ((int)(((byte)(128)))));
+            this.lblAsignaturas.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblAsignaturas.Location = new System.Drawing.Point(567, 366);
+            this.lblAsignaturas.Margin = new System.Windows.Forms.Padding(6, 0, 6, 0);
+            this.lblAsignaturas.Name = "lblAsignaturas";
+            this.lblAsignaturas.Size = new System.Drawing.Size(250, 29);
+            this.lblAsignaturas.TabIndex = 2;
+            this.lblAsignaturas.Text = "Asignaturas y Notas:";
+            // 
+            // txtCarnet
+            // 
+            this.txtCarnet.BackColor = System.Drawing.SystemColors.InactiveCaption;
+            this.txtCarnet.Location = new System.Drawing.Point(227, 71);
+            this.txtCarnet.Margin = new System.Windows.Forms.Padding(6);
+            this.txtCarnet.Name = "txtCarnet";
+            this.txtCarnet.Size = new System.Drawing.Size(322, 31);
+            this.txtCarnet.TabIndex = 3;
+            // 
+            // txtNombre
+            // 
+            this.txtNombre.BackColor = System.Drawing.SystemColors.InactiveCaption;
+            this.txtNombre.Location = new System.Drawing.Point(824, 69);
+            this.txtNombre.Margin = new System.Windows.Forms.Padding(6);
+            this.txtNombre.Name = "txtNombre";
+            this.txtNombre.Size = new System.Drawing.Size(313, 31);
+            this.txtNombre.TabIndex = 4;
+            // 
+            // dgvAsignaturas
+            // 
+            this.dgvAsignaturas.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.Fill;
+            this.dgvAsignaturas.BackgroundColor = System.Drawing.Color.Silver;
+            this.dgvAsignaturas.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgvAsignaturas.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.colAsignatura,
+            this.colNotas});
+            this.dgvAsignaturas.GridColor = System.Drawing.SystemColors.GrayText;
+            this.dgvAsignaturas.Location = new System.Drawing.Point(394, 416);
+            this.dgvAsignaturas.Margin = new System.Windows.Forms.Padding(6);
+            this.dgvAsignaturas.Name = "dgvAsignaturas";
+            this.dgvAsignaturas.RowHeadersVisible = false;
+            this.dgvAsignaturas.RowHeadersWidth = 82;
+            this.dgvAsignaturas.Size = new System.Drawing.Size(621, 288);
+            this.dgvAsignaturas.TabIndex = 5;
+            // 
+            // colAsignatura
+            // 
+            this.colAsignatura.HeaderText = "Asignatura";
+            this.colAsignatura.MinimumWidth = 10;
+            this.colAsignatura.Name = "colAsignatura";
+            // 
+            // colNotas
+            // 
+            this.colNotas.HeaderText = "Notas";
+            this.colNotas.MinimumWidth = 10;
+            this.colNotas.Name = "colNotas";
+            // 
+            // btnGuardar
+            // 
+            this.btnGuardar.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(128)))), ((int)(((byte)(128)))));
+            this.btnGuardar.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnGuardar.Location = new System.Drawing.Point(596, 726);
+            this.btnGuardar.Margin = new System.Windows.Forms.Padding(6);
+            this.btnGuardar.Name = "btnGuardar";
+            this.btnGuardar.Size = new System.Drawing.Size(206, 46);
+            this.btnGuardar.TabIndex = 6;
+            this.btnGuardar.Text = "Guardar";
+            this.btnGuardar.UseVisualStyleBackColor = false;
+            this.btnGuardar.Click += new System.EventHandler(this.btnGuardar_Click);
+            // 
+            // Form2
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(12F, 25F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1600, 865);
+            this.Controls.Add(this.btnGuardar);
+            this.Controls.Add(this.dgvAsignaturas);
+            this.Controls.Add(this.txtNombre);
+            this.Controls.Add(this.txtCarnet);
+            this.Controls.Add(this.lblAsignaturas);
+            this.Controls.Add(this.lblNombre);
+            this.Controls.Add(this.lblCarnet);
+            this.Margin = new System.Windows.Forms.Padding(6);
+            this.Name = "Form2";
+            this.Text = "Form2";
+            this.Load += new System.EventHandler(this.Form2_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dgvAsignaturas)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblCarnet;
+        private System.Windows.Forms.Label lblNombre;
+        private System.Windows.Forms.Label lblAsignaturas;
+        private System.Windows.Forms.TextBox txtCarnet;
+        private System.Windows.Forms.TextBox txtNombre;
+        private System.Windows.Forms.DataGridView dgvAsignaturas;
+        private System.Windows.Forms.Button btnGuardar;
+        private System.Windows.Forms.DataGridViewTextBoxColumn colAsignatura;
+        private System.Windows.Forms.DataGridViewTextBoxColumn colNotas;
+    }
+}
---
+++ MDIEstudiantes/Form3.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace MDIEstudiantes
+{
+    public partial class Form3 : Form
+    {
+        public Form3()
+        {
+            InitializeComponent();
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Form3_Load(object sender, EventArgs e)
+        {
+            dgvDatos.Rows.Clear();
+
+            foreach (var est in DatosCompartidos.ListaEstudiantes)
+            {
+                dgvDatos.Rows.Add(est.Carnet, est.Nombre, est.Promedio().ToString("0.00"));
+            }
+        }
+    }
+}
